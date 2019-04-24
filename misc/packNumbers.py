a23383eeadcb6e98d00adc308237bfb7c645f4e0
a-yasinsky <anatoly@yasinsky.pro>
2019-04-24 16:14:12 +0600

    pack Numbers more pythonic list

--- misc/packNumbers.py
+++ misc/packNumbers.py
@@ -2,12 +2,7 @@ def packNumbers(arr):
     valMap = {x:0 for x in arr}
     for val in arr:
         valMap[val] += 1
-    retList = []
-    for key in valMap:
-        if valMap[key] > 1:
-            retList.append("{}:{}".format(key,valMap[key]))
-        else:
-            retList.append(key)
+    retList = ["{}:{}".format(key,valMap[key]) if valMap[key] > 1 else key for key in valMap]
     return retList
 
 print(packNumbers([8,5,5,5,7,7,3,4,7]))
