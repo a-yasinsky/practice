9fa000dcb758eeea6ded9e18b620b82fbc9eecce
a-yasinsky <anatoly@yasinsky.pro>
2019-05-01 14:31:10 +0600

    wordLadder with backTrack

--- misc/wordLadder.py
+++ misc/wordLadder.py
@@ -10,20 +10,26 @@ def ladderLength(beginWord, endWord, wordList):
     visited.add(beginWord)
     queue.insert(0,beginWord)
     pathLength = 0
+    backtrackMap = {}
     while queue:
         w = queue.pop()
         pathLength += 1
         words = findOneLetterDiffWords(w,wordList)
         for oneW in words:
             if oneW == endWord:
-                return pathLength
+                retList = [endWord]
+                while w:
+                    retList.insert(0,w)
+                    w = backtrackMap.get(w,None)
+                return retList
             if oneW not in visited:
                 visited.add(oneW)
                 queue.insert(0,oneW)
+                backtrackMap[oneW] = w
     return None
 
 def findOneLetterDiffWords(word, wordList):
     return [w for w in wordList if sum(a!=b for a,b in zip(word,w)) == 1]
 
-print(findOneLetterDiffWords('hit',["hot","dot","dog","lot","log","cog"] ))
-#print(ladderLength('hit','cog',["hot","dot","dog","lot","log","cog"]))
+#print(findOneLetterDiffWords('hit',["hot","dot","dog","lot","log","cog"] ))
+print(ladderLength('hit','cog',["hot","dot","dog","lot","log","cog"]))
