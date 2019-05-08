a11cca517e25b74e09bddde6c06b717c242210cf
a-yasinsky <anatoly@yasinsky.pro>
2019-05-08 12:17:29 +0600

    prison optimized

--- misc/prisonAfterNDays.py
+++ misc/prisonAfterNDays.py
@@ -4,11 +4,19 @@ def prisonAfterNDays(cells, N):
     :type N: int
     :rtype: List[int]
     """
+    state = []
     def nextDay(cells):
         return [int(cells[x-1] == cells[x+1]) if x > 0 and x < 7 else 0 for x,v in enumerate(cells)]
+
     while N > 0:
         cells = nextDay(cells)
-        N -= 1
+        if cells not in state:
+            state.append(cells)
+            N -= 1
+        else:
+            delim = state.index(cells) + 1
+            N = N % delim
+            return state[N-1]
     return cells
 
 
